a37c3df5037f32ed20a71ed8c95c1da2923e8834
gterrien <gterrien1@gmail.com>
2016-10-17 13:56:37 -0500

    Updated for continual output of mean luma to terminal with flashlight on

--- CameraTest/CameraTest/ViewController.swift
+++ CameraTest/CameraTest/ViewController.swift
@@ -11,58 +11,55 @@ import AVFoundation
 
 class ViewController: UIViewController, UINavigationControllerDelegate, AVCaptureVideoDataOutputSampleBufferDelegate {
     var captureDevice : AVCaptureDevice?
+    var session : AVCaptureSession?
     
-    @IBAction func openCamera(_ sender: AnyObject) {
-        let session = AVCaptureSession()
-        session.sessionPreset = AVCaptureSessionPresetHigh
-        do {
-              let deviceInput = try AVCaptureDeviceInput(device: captureDevice)
-        session.beginConfiguration()
-        session.addInput(deviceInput)
-        let dataOutput = AVCaptureVideoDataOutput()
-            dataOutput.videoSettings = [(kCVPixelBufferPixelFormatTypeKey as NSString) : NSNumber(value: kCVPixelFormatType_420YpCbCr8BiPlanarFullRange)]
-        dataOutput.alwaysDiscardsLateVideoFrames = true
-        session.addOutput(dataOutput)
-        session.commitConfiguration()
-        let queue = DispatchQueue(label: "testqueue")
-        dataOutput.setSampleBufferDelegate(self, queue: queue)
-        session.startRunning()
-        }
-        catch let error as NSError {
-            NSLog("\(error)")
-        }
-        }
-
     override func viewDidLoad() {
         super.viewDidLoad()
         captureDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo) as AVCaptureDevice
+        session = AVCaptureSession()
+        session!.sessionPreset = AVCaptureSessionPresetHigh
         do {
             try captureDevice?.lockForConfiguration()
             captureDevice?.torchMode = .on
             captureDevice?.unlockForConfiguration()
+            let deviceInput = try AVCaptureDeviceInput(device: captureDevice)
+            session!.beginConfiguration()
+            session!.addInput(deviceInput)
+            let dataOutput = AVCaptureVideoDataOutput()
+            dataOutput.videoSettings = [(kCVPixelBufferPixelFormatTypeKey as NSString) : NSNumber(value: kCVPixelFormatType_420YpCbCr8BiPlanarFullRange)]
+            dataOutput.alwaysDiscardsLateVideoFrames = true
+            session!.addOutput(dataOutput)
+            session!.commitConfiguration()
+            let queue = DispatchQueue(label: "testqueue")
+            dataOutput.setSampleBufferDelegate(self, queue: queue)
+            session!.startRunning()
+            
         } catch let error as NSError {
             NSLog("\(error)")
         }
-
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputSampleBuffer sampleBuffer: CMSampleBuffer!, from connection: AVCaptureConnection!) {
-        let buffer = CMSampleBufferGetImageBuffer(sampleBuffer)!
+        let buffer : CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer)!
         CVPixelBufferLockBaseAddress(buffer, CVPixelBufferLockFlags(rawValue: CVOptionFlags(0)))
+        
         let baseAddress = CVPixelBufferGetBaseAddressOfPlane(buffer, 0)
-        let ui8 = baseAddress?.load(as: UInt8.self)
-        print(ui8)
-        let newPointer = baseAddress?.advanced(by: 1)
-        let ui82 = newPointer?.load(as: UInt8.self)
-        print(ui82)
-        
-        
+        let pointer = baseAddress?.assumingMemoryBound(to: UInt8.self)
+        let bytesPerRow = CVPixelBufferGetBytesPerRowOfPlane(buffer, 0)
+        let byteBuffer = UnsafeMutablePointer<UInt8>(pointer)!
+        var sum = 0
+        let pixels = 1080 * bytesPerRow
+        for index in 0...pixels-1 {
+            sum += Int(byteBuffer[index])
         }
+        print(Double(sum)/Double(pixels))
+    }
 }
 
