6796df2a5705f2468192781253f4a9f8e6ab3b51
gterrien <gterrien1@gmail.com>
2016-10-27 11:55:41 -0500

    Hooked up brightness data to viterbi

--- CameraTest/CameraTest/ViewController.swift
+++ CameraTest/CameraTest/ViewController.swift
@@ -14,9 +14,12 @@ class ViewController: UIViewController, UINavigationControllerDelegate, AVCaptur
     var session : AVCaptureSession?
     var stateQueue : YChannelStateQueue?
     
+    var observation : [Int]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         initializeCamera()
+        observation = [Int]()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
@@ -66,7 +69,77 @@ class ViewController: UIViewController, UINavigationControllerDelegate, AVCaptur
             sum += Int(byteBuffer[index])
         }
         stateQueue?.addValue(value: Double(sum)/Double(pixels))
-        print(stateQueue?.getState())
+        
+        if (stateQueue?.getState() != -1) {
+            observation!.append((stateQueue?.getState())!)
+        }
+        
+        if (observation!.count == 90) {
+            let trans = [[0.6773,0.3227],[0.0842,0.9158]]
+            let emit = [[0.7689,0.0061,0.1713,0.0537],
+                        [0.0799,0.6646,0.1136,0.1420]]
+            let p = [0.2, 0.8]
+            let states = [0,1]
+            // 4 obs, increasing, decreasing, local max and local min
+            print(viterbi(obs:observation!, trans:trans, emit:emit, states:states, initial:p))
+            let calendar = NSCalendar.current
+            let seconds = calendar.component(.second, from: NSDate() as Date)
+            print(seconds)
+            observation!.removeAll()
+        }
+
     }
+    
+    func viterbi(obs:Array<Int>, trans:Array<Array<Double>>, emit:Array<Array<Double>>, states: Array<Int>, initial:Array<Double>)->(Double, [Int]){
+        
+        var vit = [[Int:Double]()]
+        var path = [Int:[Int]]()
+        
+        for s in states{
+            vit[0][s] = initial[s] * emit[s][obs[0]]
+            path[s] = [s]
+        }
+        for i in 1..<obs.count{
+            vit.append([:])
+            var newPath = [Int:[Int]]()
+            
+            for state1 in states{
+                var transMax = DBL_MIN
+                var maxProb = DBL_MIN
+                var bestState:Int?
+                
+                //
+                for state2 in states{
+                    let transProb = vit[i-1][state2]! * trans[state2][state1]
+                    if transProb > transMax{
+                        transMax = transProb
+                        maxProb = transMax * emit[state1][obs[i]]
+                        vit[i][state1] = maxProb
+                        bestState = state2
+                        newPath[state1] = path[bestState!]! + [state1]
+                    }
+                }
+                
+            }
+            path = newPath
+        }
+        //print(vit)
+        //print(path)
+        let len = obs.count - 1
+        var bestState:Int?
+        var maxProb = DBL_MIN
+        for state in states{
+            if vit[len][state]! > maxProb{
+                maxProb = vit[len][state]!
+                bestState = state
+            }
+        }
+        
+        
+        
+        return (maxProb, path[bestState!]!)
+        
+    }
+
 }
 
