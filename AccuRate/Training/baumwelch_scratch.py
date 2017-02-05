58056264e1e73f016339eec5632a783789f100ff
Aidan Holloway-Bidwell <john@eduroam-169-53.dyn.carleton.edu>
2017-02-04 20:07:43 -0600

    Very close to working implementation of baum welch in baumwelch_scratch

--- AccuRate/Training/baumwelch_scratch.py
+++ AccuRate/Training/baumwelch_scratch.py
@@ -33,31 +33,38 @@ def beta(A, B, observations):
 	# Assuming if a state is last, it has a 1.0 prob of being last.
 	# If this is not true, line below may have to be changed.
 	bw[:,-1] = 1.0
+
 	for t in range(T-2, -1, -1):
-		for j in range(num_states):
-			for i in range(num_states):
-				bw[j,t] += bw[i,t+1] * A[i,j]
-			bw[j,t] *= B[j,observations[t]]
+		for i in range(num_states):
+			for j in range(num_states):
+				bw[i,t] += bw[j,t+1] * A[i,j] * B[j,observations[t + 1]]
 
 	return bw
 
-def xi(A, B, alpha, beta, prob_obs, observations):
+# Calculate probability of going from state i to j, given probability sequence,
+# at any time t
+def xi(A, B, alpha, beta, observations):
+	prob = prob_obs(alpha)
+
 	num_states = A.shape[0]
 	T = len(observations)
 	xi = np.zeros((T, num_states, num_states))
 
 	for i_ind in range(num_states):
 		for j_ind in range(num_states):
-			for t_ind in range(T):
+			for t_ind in range(T - 1):
 				not_quite_xi = \
-				alpha[i_ind,t_ind] * \
-				beta[j_ind,t_ind] * \
-				A[i_ind,j_ind] * \
-				B[j_ind, observations[t_ind]]
+					alpha[i_ind,t_ind] * \
+					beta[j_ind,t_ind + 1] * \
+					A[i_ind,j_ind] * \
+					B[j_ind, observations[t_ind + 1]]
 
-				xi[t_ind,i_ind,j_ind] = not_quite_xi / prob_obs
+				xi[t_ind,i_ind,j_ind] = not_quite_xi / prob
 
 	return xi
+
+def prob_obs(alpha):
+	return sum(alpha.T[-1])
 
 def a_hat(xi):
 	T, num_states = xi.shape[:2]
@@ -65,30 +72,36 @@ def a_hat(xi):
 
 	for i in range(num_states):
 		for j in range(num_states):
-			a_hat[i, j] = np.sum(xi[:, i, j]) / np.sum(xi[:, i, :])
+			a_hat[i, j] = np.sum(xi[0:T, i, j]) / np.sum(xi[0:T, i, :])
 
 	return a_hat
 
-def gamma(alpha, beta, prob_obs, observations):
+# Calculate the probability of being in state j at time t
+def gamma(alpha, beta, observations):
+	prob = prob_obs(alpha)
+
 	T = len(observations)
 	num_states = alpha.shape[0]
 	gamma = np.zeros((T, num_states))
 
 	for t_ind in range(T):
 		for j_ind in range(num_states):
-			gamma[t_ind, j_ind] = alpha[j_ind, t_ind] * beta[j_ind, t_ind] / prob_obs
+			gamma[t_ind, j_ind] = alpha[j_ind, t_ind] * beta[j_ind, t_ind] / prob
 
 	return gamma
 
 def b_hat(B, gamma, observations):
 	num_states, num_obs = B.shape
+	T = len(observations)
+
 	b_hat = np.zeros((num_states, num_obs))
 
 	for i_ind in range(num_states):
 		for o_ind in range(num_obs):
-			#print("i_ind: ", i_ind, " o_ind: ", o_ind)
-			cur_obs_ind = np.array(np.where(observations == o_ind)) + 1
-			b_hat[i_ind, o_ind] = np.sum(gamma[cur_obs_ind, i_ind]) / np.sum(gamma[:, i_ind])
+			for t_ind in range(T):
+				if observations[t_ind] == o_ind:
+					b_hat[i_ind, o_ind] += gamma[t_ind, i_ind]
+			b_hat[i_ind, o_ind] /= sum(gamma[:, i_ind])
 
 	return b_hat
 
@@ -104,16 +117,8 @@ def baum_welch(A, B, observations):
 		alph = alpha(curA, curB, observations)
 		bet = beta(curA, curB, observations)
 
-		print("Ours, iteration ", count, ": alpha = ", alph, ", beta = ", bet)
-
-		prob_obs = 0
-		for t_ind in range(T):
-			for k in range(num_states):
-				prob_obs += alph[k, t_ind] * bet[k, t_ind]
-
-		x = xi(curA, curB, alph, bet, prob_obs, observations)
-
-		gam = gamma(alph, bet, prob_obs, observations)
+		x = xi(curA, curB, alph, bet, observations)
+		gam = gamma(alph, bet, observations)
 
 		oldA, oldB = curA, curB
 
@@ -128,6 +133,8 @@ def baum_welch(A, B, observations):
 
 	return curA, curB
 
+
+# Deprecated by the tests.py module
 def test():
 	A = np.array([0.6794, 0.3206, 0.0, 0.0, \
 				  0.0, 0.5366, 0.4634, 0.0, \
@@ -146,7 +153,17 @@ def test():
 
 	O = np.array([0,3,1,2,1,0,2,3,0,1,0,2,0,1,2,2,0,3,0,0,2,0,0,1,2,0,1,2,0])
 
-	print(baum_welch(A,B,O))
+	alph = alpha(A, B, O)
+	bet = beta(A, B, O)
+
+	print(alph.T)
+	print(bet.T)
+
+	T = len(O)
+	num_states, num_obs = B.shape
+
+	#print(xi(A, B, alph, bet, prob_obs, O))
+	#print(baum_welch(A,B,O))
 
 
 
--- AccuRate/Training/tests.py
+++ AccuRate/Training/tests.py
@@ -6,6 +6,7 @@ Created on Nov 13, 2012
 
 #from GMHMM import GMHMM
 from DiscreteHMM import DiscreteHMM
+import baumwelch_scratch as bw
 import numpy
 
 """def test_simple():
@@ -84,6 +85,7 @@ def test_rand():
 def test_discrete():
 
     ob5 = (0,3,1,2,1,0,2,3,0,1,0,2,0,1,2,2,0,3,0,0,2,0,0,1,2,0,1,2,0)
+    T = len(ob5)
     #print "Doing Baum-welch"
     
     #atmp = numpy.random.random_sample((4, 4))
@@ -110,9 +112,40 @@ def test_discrete():
     pi = numpy.array([0.25, 0.20, 0.10, 0.45])
     
     hmm2 = DiscreteHMM(4,4,a,b,pi,init_type='user',precision=numpy.longdouble,verbose=True)
-    #print hmm2.alpha(ob5)
-    #print hmm2.beta(ob5)
-    hmm2.xi(ob5)
+
+    alpha1 = bw.alpha(a, b, ob5)
+    beta1 = bw.beta(a, b, ob5)
+
+    alpha2 = hmm2.alpha(ob5)
+    beta2 = hmm2.beta(ob5)
+
+    # Test if probabilities of obs sequences add up to 1
+    total = 0
+    for i in [0,1,2,3]:
+        for j in [0,1,2,3]:
+            for k in [0,1,2,3]:
+                for l in [0,1,2,3]:
+                    inp = (i,j,k,l)
+                    alph = bw.alpha(a, b, inp)
+                    prob = bw.prob_obs(alph)
+                    total += prob
+
+    print(total)
+
+    xi1 = bw.xi(a, b, alpha1, beta1, ob5)
+    xi2 = hmm2.xi(ob5)
+
+    gamma1 = bw.gamma(alpha1, beta1, ob5)
+    gamma2 = hmm2._calcgamma(xi2, T)
+
+    new_alpha1 = bw.a_hat(xi1)
+    new_alpha2 = hmm2._reestimateA(ob5, xi2, gamma2)
+
+    new_beta1 = bw.b_hat(b, gamma1, ob5)
+    new_beta2 = hmm2._reestimateB(ob5, gamma1)
+
+    print(xi1 - xi2)
+
     #hmm2.train(numpy.array(ob5),iterations=10000,epsilon=.00001)
     #print "Pi",hmm2.pi
     #print "A",hmm2.A
