1f5404dd62fa9e147d337eae5864ec58b3477770
gterrien <gterrien1@gmail.com>
2017-02-12 21:13:37 -0600

    Ripped out all 'brightness heuristic' code; Attempted to remove optionals as much as possible

--- AccuRate/CameraTest/CameraTest/ViewController.swift
+++ AccuRate/CameraTest/CameraTest/ViewController.swift
@@ -45,56 +45,45 @@ class ViewController: UIViewController, UINavigationControllerDelegate, AVCaptur
     let MIN_LUMA_MEAN = Double(60)
     let MAX_LUMA_STD_DEV = Double(20)
     
-    var brightnesses : [Double]?
+    var stateQueue : YChannelStateQueue = YChannelStateQueue()
+    var heartRates : [Int] = [Int]()
+    var brightnessDerivatives : [Int] = [Int]()
     
-    var stateQueue : YChannelStateQueue?
-    var heartRates : [Int]?
-    var brightnessDerivatives : [Int]?
+    var obsTime : [Double] = [Double]()
+    var beginningTime : Double = 0
+    var stateCount : Int = 0
+    var bpmRecords : [Int] = [0,0,0,0,0,0]
+    var HRCount : Int = 0
+    var isFirstHR : Bool = true
+    var lastCalculated : Date = Date()
     
-    var obsTime : [Double]?
-    var beginningTime : Double?
-    var stateCount : Int?
-    var bpmRecords : [Int]?
-    var HRCount : Int?
-    var isFirstHR:Bool?
-    var lastCalculated : Date?
+    var previousBPM : Int = 0
     
-    var previousBPM: Int?
-    var previousBrightnessDifference : Double?
+    var camCoverStartTime : Double = 0
+    var camCoverStartIndex : Int = 0
+    var needToFindNextPeak : Bool = false
+    var nextPeakIndex : Int = 0
+    var tempObservation : [Int] = [Int]()
+    var tempObsTime : [Double] = [Double]()
     
-    var previousCover : Bool?
-    var camCoverStartTime : Double?
-    var camCoverStartIndex : Int?
-    var needToFindNextPeak : Bool?
-    var nextPeakIndex : Int?
-    var tempObservation : [Int]?
-    var tempObsTime : [Double]?
-    var tempBrightness : [Double]?
-    var previousMaxBrightness : Double?
+    var currentBPM : Int = 0
+    var uiTimer : Timer = Timer()
     
-    var currentBPM : Int?
-    var uiTimer : Timer?
-    
-    var previousMeasuredBPM : Int?
+    var previousMeasuredBPM : Int = 0
     
     func initialize() {
         stateQueue = YChannelStateQueue()
         heartRates = [Int]()
         brightnessDerivatives = [Int]()
-        brightnesses = [Double]()
         obsTime = [Double]()
-        previousBrightnessDifference = -1
         bpmRecords = [0,0,0,0,0,0]
         HRCount = 0
         isFirstHR = true
         previousBPM = 0
-        previousCover = false
         camCoverStartTime = 0.0
         nextPeakIndex = 0
         tempObservation = []
         tempObsTime = []
-        tempBrightness = []
-        previousMaxBrightness = -1.0
         currentBPM = 0
         previousMeasuredBPM = 0
     }
@@ -123,10 +112,10 @@ class ViewController: UIViewController, UINavigationControllerDelegate, AVCaptur
     }
     
     func updateDisplayedBPM() {
-        if self.currentBPM! != 0 {
+        if self.currentBPM != 0 {
             DispatchQueue.main.async {
-                self.BPMText.text = String(self.currentBPM!) + " BPM"
-                if self.currentBPM! > 100 {
+                self.BPMText.text = String(self.currentBPM) + " BPM"
+                if self.currentBPM > 100 {
                     self.BPMText.frame.size.width = 190
                     self.heartView.removeFromSuperview()
                     self.displayHeart(imageName: "Heart_normal")
@@ -157,7 +146,7 @@ class ViewController: UIViewController, UINavigationControllerDelegate, AVCaptur
         else {
             heartView.removeFromSuperview()
             displayHeart(imageName: "Heart_inactive")
-            uiTimer?.invalidate()
+            uiTimer.invalidate()
             // End camera processes
             session!.stopRunning()
             toggleFlashlight()
@@ -353,8 +342,8 @@ class ViewController: UIViewController, UINavigationControllerDelegate, AVCaptur
             for state1 in states{
                 var transMax = DBL_MIN
                 var maxProb = DBL_MIN
-                var bestState:Int?
-    
+                var bestState : Int = 0
+                
                 for state2 in states{
                     let transProb = vit[i-1][state2]! * trans[state2][state1]
                     if transProb > transMax{
@@ -362,7 +351,7 @@ class ViewController: UIViewController, UINavigationControllerDelegate, AVCaptur
                         maxProb = transMax * emit[state1][obs[i]]
                         vit[i][state1] = maxProb
                         bestState = state2
-                        newPath[state1] = path[bestState!]! + [state1]
+                        newPath[state1] = path[bestState]! + [state1]
                     }
                 }
                 
@@ -370,7 +359,7 @@ class ViewController: UIViewController, UINavigationControllerDelegate, AVCaptur
             path = newPath
         }
         let len = obs.count - 1
-        var bestState:Int?
+        var bestState:Int = 0
         var maxProb = DBL_MIN
         for state in states{
             if vit[len][state]! > maxProb{
@@ -378,52 +367,45 @@ class ViewController: UIViewController, UINavigationControllerDelegate, AVCaptur
                 bestState = state
             }
         }
-        return (maxProb, path[bestState!]!)
+        return (maxProb, path[bestState]!)
         
     }
-  
-    func calculate(states:Array<Int>, brightnesses: [Double]){
-        var previous = (states[0], brightnesses[0])
+    
+    func calculate(states:Array<Int>){
+        var previous = states[0]
         var validBPM = 0
         var tempBPM = 0
         var interval = 0.0
-        var brightnessDifference = Double(-1)
         
         for i in 0..<states.count{
-            if states[i] == 3 && previous.0 == 2 {
-                brightnessDifference = abs(previous.1-brightnesses[i])
-                if previousBrightnessDifference == -1 {
-                    previousBrightnessDifference = brightnessDifference
-                }
-            }
-            if (states[i]==0 && previous.0 == 3) {
+            if (states[i]==0 && previous == 3) {
                 if beginningTime != nil {
-                    interval = (obsTime?[i])! - beginningTime!
+                    interval = (obsTime[i]) - beginningTime
                     validBPM = Int(60 / interval)
-                    bpmRecords?[HRCount! % 6] = validBPM
-                    if HRCount! >= 6{
-                        tempBPM = (validBPM + previousBPM!)/2
+                    bpmRecords[HRCount % 6] = validBPM
+                    if HRCount >= 6{
+                        tempBPM = (validBPM + previousBPM)/2
                         var avg:Double = 0
                         var sum:Double = 0
-                        var tempRecords = bpmRecords?.sorted()
+                        var tempRecords = bpmRecords.sorted()
                         for k in 1..<5{
-                            sum = sum + Double((tempRecords?[k])!)
+                            sum = sum + Double((tempRecords[k]))
                         }
                         avg = sum/4
                         var usefulEnds:Int = 0
-                        if abs(Double((tempRecords?[0])!) - avg)<=10{
-                            sum = sum + Double((tempRecords?[0])!)
+                        if abs(Double((tempRecords[0])) - avg)<=10{
+                            sum = sum + Double((tempRecords[0]))
                             usefulEnds += 1
                         }
-                        if abs(Double((tempRecords?[5])!) - avg)<=10{
-                            sum = sum + Double((tempRecords?[5])!)
+                        if abs(Double((tempRecords[5])) - avg)<=10{
+                            sum = sum + Double((tempRecords[5]))
                             usefulEnds += 1
                         }
                         avg = sum / Double(4 + usefulEnds)
                         if (abs(tempBPM - Int(avg)) <= 10)
                             && (tempBPM >= 30) && (tempBPM <= 300) {
                             previousBPM = tempBPM
-                            validBPM = ((tempRecords?[2])!+(tempRecords?[3])!+tempBPM) / 3
+                            validBPM = ((tempRecords[2])+(tempRecords[3])+tempBPM) / 3
                         } else {
                             validBPM = Int(avg)
                         }
@@ -431,19 +413,17 @@ class ViewController: UIViewController, UINavigationControllerDelegate, AVCaptur
                     } else{
                         previousBPM = validBPM
                     }
-                    HRCount = HRCount! + 1
+                    HRCount = HRCount + 1
                 }
-    
-                beginningTime = obsTime?[i]
+                
+                beginningTime = obsTime[i]
                 
             }
             
-            previous = (states[i], brightnesses[i])
-            previousBrightnessDifference = brightnessDifference
+            previous = states[i]
         }
-
     }
-
+    
     
     func useCaptureOutputForHeartRateEstimation(mean: Double, bytesPerRow: Int) {
         let currentTime = NSDate().timeIntervalSince1970
@@ -462,54 +442,45 @@ class ViewController: UIViewController, UINavigationControllerDelegate, AVCaptur
         if (self.camCovered) {
             let pixels = 1080 * bytesPerRow
             let value = mean/Double(pixels)
-            stateQueue?.addValue(value: value)
-            if (self.tempObservation?.count != 0){
-                if (((tempObsTime?.last!)! - (tempObsTime?.first!)!) >= 2.0) {
-                    for i in 0..<self.tempObservation!.count {
-                        if (((tempObsTime?.last!)! - (tempObsTime?[self.tempObsTime!.count - i - 1])!) >= 1.0) {
-                            self.brightnessDerivatives? = self.brightnessDerivatives! + tempObservation!
-                            self.obsTime? = self.obsTime! + tempObsTime!
-                            self.brightnesses? = self.brightnesses! + tempBrightness!
+            stateQueue.addValue(value: value)
+            if (self.tempObservation.count != 0){
+                if (((tempObsTime.last!) - (tempObsTime.first!)) >= 2.0) {
+                    for i in 0..<self.tempObservation.count {
+                        if (((tempObsTime.last!) - (tempObsTime[self.tempObsTime.count - i - 1])) >= 1.0) {
+                            self.brightnessDerivatives = self.brightnessDerivatives + tempObservation
+                            self.obsTime = self.obsTime + tempObsTime
                         } else {
-                            self.tempBrightness?.removeLast(i)
-                            self.tempObsTime?.removeLast(i)
-                            self.tempObservation?.removeLast(i)
+                            self.tempObsTime.removeLast(i)
+                            self.tempObservation.removeLast(i)
                         }
                     }
                 }
                 tempObsTime = []
                 tempObservation = []
-                tempBrightness = []
             }
             self.camCoverStartTime = currentTime
             self.needToFindNextPeak = true
-            if (self.brightnessDerivatives?.count != 0){
-                if (((self.obsTime?.last)! - (self.obsTime?.first)!) >= 3.0){
-                    self.calculate(states: self.viterbi(obs:self.brightnessDerivatives!, trans:trans, emit:emit, states:states, initial:p).1, brightnesses: brightnesses!)
+            if (self.brightnessDerivatives.count != 0){
+                if (((self.obsTime.last)! - (self.obsTime.first)!) >= 3.0){
+                    self.calculate(states: self.viterbi(obs:self.brightnessDerivatives, trans:trans, emit:emit, states:states, initial:p).1)
                 }
             }
             
-            self.tempObservation!.append((stateQueue?.getState())!.0)
-            self.tempBrightness!.append((stateQueue?.getState())!.1)
-            self.tempObsTime!.append(currentTime)
-            if (needToFindNextPeak!) {
-                if ((currentTime - self.camCoverStartTime!) >= 1.0) {
+            self.tempObservation.append((stateQueue.getState()))
+            self.tempObsTime.append(currentTime)
+            if (needToFindNextPeak) {
+                if ((currentTime - self.camCoverStartTime) >= 1.0) {
                     self.needToFindNextPeak = false
                 } else {
-                    if (self.previousMaxBrightness! < (tempBrightness?.last!)!) {
-                        self.previousMaxBrightness! = (tempBrightness?.last!)!
-                        tempObsTime = [(tempObsTime?.last!)!]
-                        tempObservation = [(tempObservation?.last!)!]
-                        tempBrightness = [(tempBrightness?.last!)!]
-                    }
+                    tempObsTime = [(tempObsTime.last!)]
+                    tempObservation = [(tempObservation.last!)]
                 }
                 
             }
             
-            if (stateQueue?.getState().0 != -1) {
-                obsTime!.append(NSDate().timeIntervalSince1970)
-                brightnesses!.append((stateQueue?.getState().1)!)
-                brightnessDerivatives!.append((stateQueue?.getState())!.0)
+            if (stateQueue.getState() != -1) {
+                obsTime.append(NSDate().timeIntervalSince1970)
+                brightnessDerivatives.append((stateQueue.getState()))
             }
             
         }
--- AccuRate/CameraTest/CameraTest/YChannelStateQueue.swift
+++ AccuRate/CameraTest/CameraTest/YChannelStateQueue.swift
@@ -23,25 +23,25 @@ class YChannelStateQueue: NSObject {
         valuesArray?.insert(value, at: 0)
     }
     
-    func getState() -> (Int, Double) {
+    func getState() -> Int {
         // These are arbitrary numbers and I'm not sure if my labels for rising/falling/min/max are correct.
         if (valuesArray?.count)! < 3 {
-            return (-1, -1) // not enough measurements yet
+            return -1 // not enough measurements yet
         }
         if valuesArray![0] >= valuesArray![1] && valuesArray![1] >= valuesArray![2] {
-            return (0, (valuesArray?[1])!) // rising
+            return 0 // rising
         }
         if valuesArray![0] <= valuesArray![1] && valuesArray![1] <= valuesArray![2] {
-            return (1, (valuesArray?[1])!) //falling
+            return 1 //falling
         }
         if valuesArray![0] <= valuesArray![1] && valuesArray![1] >= valuesArray![2] {
-            return (2, (valuesArray?[1])!) // local minimum
+            return 2 // local maximum
         }
         if valuesArray![0] >= valuesArray![1] && valuesArray![1] <= valuesArray![2] {
-            return (3, (valuesArray?[1])!) // local maximum
+            return 3 // local minimum
         }
         
-        return (-1, -1); // hopefully don't get here
+        return -1 // hopefully don't get here
     }
 
 }
