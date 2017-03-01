d78e38c7232de97ae4fda5af24875384b30a385f
Aidan Holloway-Bidwell <john@eduroam-169-53.dyn.carleton.edu>
2017-03-01 14:38:19 -0600

    Messing around just to make figures for presentation

--- AccuRate/Training/baumwelch_scratch.py
+++ AccuRate/Training/baumwelch_scratch.py
@@ -5,6 +5,8 @@ import csv
 from DiscreteHMM import DiscreteHMM
 import sys
 import fancy_bw as fancy
+from matplotlib import pyplot as plt
+import math
 
 # calculate forward probability matrix
 def alpha(A, B, pi, observations):
@@ -114,14 +116,44 @@ def baum_welch(A, B, pi, observations):
 	cur_pi = pi
 	num_states, num_obs = B.shape
 	T = len(observations)
-	pastProb = 0
+
+	ind = []
+	p = []
+	A1, A2 ,A3 ,A4 ,A5 ,A6 ,A7 ,A8 = [], [], [], [], [], [], [], []
+	B1, B2, B3, B4, B5, B6, B7, B8, B9, B10, B11, B12, B13, B14, B15, B16 = [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []
 
 	count = 1
 	while True:
 		alph = alpha(curA, curB, cur_pi, observations)
 		bet = beta(curA, curB, observations)
 
-		curProb = prob_obs(alph)
+		ind.append(count)
+		p.append(math.log(prob_obs(alph),10))
+		A1.append(curA[0][0])
+		A2.append(curA[0][1])
+		A3.append(curA[1][1])
+		A4.append(curA[1][2])
+		A5.append(curA[2][2])
+		A6.append(curA[2][3])
+		A7.append(curA[3][3])
+		A8.append(curA[3][0])
+		B1.append(curB[0][0])
+		B2.append(curB[0][1])
+		B3.append(curB[0][2])
+		B4.append(curB[0][3])
+		B5.append(curB[1][0])
+		B6.append(curB[1][1])
+		B7.append(curB[1][2])
+		B8.append(curB[1][3])
+		B9.append(curB[2][0])
+		B10.append(curB[2][1])
+		B11.append(curB[2][2])
+		B12.append(curB[2][3])
+		B13.append(curB[3][0])
+		B14.append(curB[3][1])
+		B15.append(curB[3][2])
+		B16.append(curB[3][3])
+
 		#for r in alph.T:
 			#print(r)
 		x = xi(curA, curB, alph, bet, observations)
@@ -133,16 +165,48 @@ def baum_welch(A, B, pi, observations):
 		curB = b_hat(oldB, gam, observations)
 		cur_pi = gam[0]
 
-		if (curProb == pastProb) or (np.linalg.norm(oldA - curA) < .00000001 and np.linalg.norm(oldB - curB) < .00000001):
+		if np.linalg.norm(oldA - curA) < .00001 and np.linalg.norm(oldB - curB) < .00001:
 			break
-
-		pastProb = curProb
 
 		count += 1
 		print(".",end='')
 		sys.stdout.flush()
 
 	print(count," iterations.")
+	f, axarr = plt.subplots(2, sharex=True)
+	axarr[0].set_axis_bgcolor('black')
+	axarr[0].set_title('Transition and Emission Matrix Values')
+	axarr[1].set_axis_bgcolor('black')
+	axarr[1].set_title('Probability of Observation Sequence (log)')
+	axarr[0].plot(ind,B1,'c')
+	axarr[0].plot(ind,B2,'c')
+	axarr[0].plot(ind,B3,'c')
+	axarr[0].plot(ind,B4,'c')
+	axarr[0].plot(ind,B5,'c')
+	axarr[0].plot(ind,B6,'c')
+	axarr[0].plot(ind,B7,'c')
+	axarr[0].plot(ind,B8,'c')
+	axarr[0].plot(ind,B9,'c')
+	axarr[0].plot(ind,B10,'c')
+	axarr[0].plot(ind,B11,'c')
+	axarr[0].plot(ind,B12,'c')
+	axarr[0].plot(ind,B13,'c')
+	axarr[0].plot(ind,B14,'c')
+	axarr[0].plot(ind,B15,'c')
+	axarr[0].plot(ind,B16,'c')
+	axarr[0].plot(ind,A1,'y')
+	axarr[0].plot(ind,A2,'y')
+	axarr[0].plot(ind,A3,'y')
+	axarr[0].plot(ind,A4,'y')
+	axarr[0].plot(ind,A5,'y')
+	axarr[0].plot(ind,A6,'y')
+	axarr[0].plot(ind,A7,'y')
+	axarr[0].plot(ind,A8,'y')
+	axarr[1].plot(ind,p,'m')
+	axarr[1].set_xlabel('Number of Iterations')
+
+	plt.show()
+	plt.savefig('baum_welch.png')
 
 	return curA, curB, cur_pi
 
@@ -227,19 +291,9 @@ def test(filepath):
 
 				print("Running our model...")
 				print("First half")
-				newA1, newB1, newPi1 = baum_welch(ourA.copy(), ourB.copy(), ourPi.copy(), O1)
+				ourA, ourB, ourPi = baum_welch(ourA.copy(), ourB.copy(), ourPi.copy(), O1)
 				print("Second half")
-				newA2, newB2, newPi2 = baum_welch(ourA.copy(), ourB.copy(), ourPi.copy(), O2)
-
-				sumA += newA1
-				sumB += newB1
-				sumPi += newPi1
-
-				sumA += newA2
-				sumB += newB2
-				sumPi += newPi2
-
-				count += 2
+				#newA2, newB2, newPi2 = baum_welch(ourA.copy(), ourB.copy(), ourPi.copy(), O2)
 				
 				#print("Running Guy's model...")
 				#hmm2 = DiscreteHMM(4,4,guyA,guyB,guyPi,init_type='user',precision=np.longdouble,verbose=True)
@@ -251,9 +305,9 @@ def test(filepath):
 
 	with open("results_alldata_vanillamatrices.txt", 'w') as file:
 		file.write("Our Results:\n")
-		file.write("A: " + str(sumA / count) + "\n")
-		file.write("B: " + str(sumB / count) + "\n")
-		file.write("Pi: " + str(sumPi / count) +  "\n\n")
+		file.write("A: " + str(ourA) + "\n")
+		file.write("B: " + str(ourB) + "\n")
+		file.write("Pi: " + str(ourPi) +  "\n\n")
 		#file.write("Guy's Results:\n")
 		#file.write("A: " + str(guyA) + "\n")
 		#file.write("B: " + str(guyB) + "\n")
