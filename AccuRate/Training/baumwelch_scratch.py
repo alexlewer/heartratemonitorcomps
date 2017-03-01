a1cd2272b0be39d0bbfbff99c60a9e42093c9cef
Aidan Holloway-Bidwell <john@eduroam-169-53.dyn.carleton.edu>
2017-02-28 21:04:25 -0600

    testing stuff

--- AccuRate/Training/baumwelch_scratch.py
+++ AccuRate/Training/baumwelch_scratch.py
@@ -6,6 +6,7 @@ from DiscreteHMM import DiscreteHMM
 import sys
 import fancy_bw as fancy
 from decimal import *
+import matplotlib.pyplot as plt
 
 # calculate forward probability matrix
 def alpha(A, B, pi, observations):
@@ -116,10 +117,42 @@ def baum_welch(A, B, pi, observations):
 	num_states, num_obs = B.shape
 	T = len(observations)
 
+	ind = []
+	p = []
+	A1, A2 ,A3 ,A4 ,A5 ,A6 ,A7 ,A8 = [], [], [], [], [], [], [], []
+	B1, B2, B3, B4, B5, B6, B7, B8, B9, B10, B11, B12, B13, B14, B15, B16 = [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []
+
 	count = 1
 	while True:
 		alph = alpha(curA, curB, cur_pi, observations)
 		bet = beta(curA, curB, observations)
+
+		ind.append(count)
+		p.append(prob_obs(alph))
+		A1.append(curA[0][0])
+		A2.append(curA[0][1])
+		A3.append(curA[1][1])
+		A4.append(curA[1][2])
+		A5.append(curA[2][2])
+		A6.append(curA[2][3])
+		A7.append(curA[3][3])
+		A8.append(curA[3][0])
+		B1.append(curB[0][0])
+		B2.append(curB[0][1])
+		B3.append(curB[0][2])
+		B4.append(curB[0][3])
+		B5.append(curB[1][0])
+		B6.append(curB[1][1])
+		B7.append(curB[1][2])
+		B8.append(curB[1][3])
+		B9.append(curB[2][0])
+		B10.append(curB[2][1])
+		B11.append(curB[2][2])
+		B12.append(curB[2][3])
+		B13.append(curB[3][0])
+		B14.append(curB[3][1])
+		B15.append(curB[3][2])
+		B16.append(curB[3][3])
 
 		#for r in alph.T:
 			#print(r)
@@ -140,6 +173,37 @@ def baum_welch(A, B, pi, observations):
 		sys.stdout.flush()
 
 	print(count," iterations.")
+	f, axarr = plt.subplots(2, sharex=True)
+	axarr[0].set_axis_bgcolor('black')
+	axarr[1].set_axis_bgcolor('black')
+	axarr[0].plot(ind,B1,'c')
+	axarr[0].plot(ind,B2,'c')
+	axarr[0].plot(ind,B3,'c')
+	axarr[0].plot(ind,B4,'c')
+	axarr[0].plot(ind,B5,'c')
+	axarr[0].plot(ind,B6,'c')
+	axarr[0].plot(ind,B7,'c')
+	axarr[0].plot(ind,B8,'c')
+	axarr[0].plot(ind,B9,'c')
+	axarr[0].plot(ind,B10,'c')
+	axarr[0].plot(ind,B11,'c')
+	axarr[0].plot(ind,B12,'c')
+	axarr[0].plot(ind,B13,'c')
+	axarr[0].plot(ind,B14,'c')
+	axarr[0].plot(ind,B15,'c')
+	axarr[0].plot(ind,B16,'c')
+	axarr[0].plot(ind,A1,'y')
+	axarr[0].plot(ind,A2,'y')
+	axarr[0].plot(ind,A3,'y')
+	axarr[0].plot(ind,A4,'y')
+	axarr[0].plot(ind,A5,'y')
+	axarr[0].plot(ind,A6,'y')
+	axarr[0].plot(ind,A7,'y')
+	axarr[0].plot(ind,A8,'y')
+	axarr[1].plot(ind,p,'r')
+
+	plt.show()
+	plt.savefig('baum_welch.png')
 
 	return curA, curB, cur_pi
 
@@ -210,9 +274,9 @@ def test(filepath):
 	#			  0.0005, 0.8440, 0.0021, 0.1534])
 
 	ourA = np.array([.5, .5, 0.0, 0.0, \
-				0.0, .5, .5, 0.0, \
-				0.0, 0.0, .5, .5, \
-				.5, 0.0, 0.0, .5])
+					0.0, .5, .5, 0.0, \
+					0.0, 0.0, .5, .5, \
+					.5, 0.0, 0.0, .5])
 
 	ourB = np.array([.25, .25, .25, .25, \
 				.25, .25, .25, .25, \
@@ -255,6 +319,7 @@ def test(filepath):
 				sumA += newA
 				sumB += newB
 				sumPi += newPi
+
 
 				count += 1
 				
